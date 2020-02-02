8c3132fe3e57d67fce874c2fee17510180ca70d0
zakir matin <zakir.matin@gmail.com>
2020-02-02 18:20:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+  
+}
+
+
+function happyHolidaysTo(name) {
+    console.log(name)
+    return "Happy holidays, ${name}!"
+    }
+
+function happyHolidaysTo(name) {
+    console.log(name)
+    return `Happy holidays, ${name}!`
+    }
