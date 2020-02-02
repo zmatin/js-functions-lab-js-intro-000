8274f31929f7acd1b0b551d8ec521f9507c2739c
zakir matin <zakir.matin@gmail.com>
2020-02-02 19:30:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
     return `Happy ${holiday}, ${name}!`
     }
+    
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+  
+}
